ed0a05ab95a57fe56cbe9c11eb4c3a63f5310699
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-26 00:29:13 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mac User on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
