32f978490d72b0096f64c8b8b829df89387010b4
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-26 08:42:18 +0100

    Project complete

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Mac User on 26/09/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
